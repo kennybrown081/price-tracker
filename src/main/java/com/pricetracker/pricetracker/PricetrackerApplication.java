e9ec74eec8d0a88953d513dfb6e869bf5af04352
chrysanthusobinna <chrysanthusobinna@gmail.com>
2025-04-28 22:28:30 +0100

    chore: initialize Spring Boot project with dependencies

---
+++ src/main/java/com/pricetracker/pricetracker/PricetrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pricetracker.pricetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricetrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricetrackerApplication.class, args);
+	}
+
+}
